a526d7d10b92539cc4fbb07dfdcff650efc88f29
Renan Nardi <renan_terra_rica@hotmail.com.br>
2022-10-06 07:34:47 -0300

    edt

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function CamaraFria(){
     var Titulo = document.querySelector("#Temperatura").textContent = "CAMARA FRIA" ;
 }
 function TunelDeCongelamento(){
-    var Titulo = document.querySelector("#Titulo").textContent = "TUNEL DE COMGELAMENTO" ;
-    var Titulo = document.querySelector("#Temperatura").textContent = "CAMARA FRIA" ;
+    var Titulo = document.querySelector("#DD").innerHTML = "" ;
+
+    
 }
